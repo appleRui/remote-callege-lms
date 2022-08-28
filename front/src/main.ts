6f9c789a9bab137f229b304a9792a8f72ed90eaf
appleRui <apple31581@gmail.com>
2022-08-28 10:49:47 +0900

    feature/#1

--- front/src/main.ts
+++ front/src/main.ts
@@ -1,5 +1,15 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import Antd from "ant-design-vue";
+import 'ant-design-vue/dist/antd.css';
 
-createApp(App).use(router).mount("#app");
+const app = createApp(App);
+
+// Register Vue-Router
+app.use(router);
+
+// Register Antd-Design-Vue
+app.use(Antd);
+
+app.mount("#app");
